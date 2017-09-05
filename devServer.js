2b6084837492d1909b0df88b1549bd9c227753d9
Vitaly Nikolaev <nikolaev.vitally@yandex.ru>
2017-09-05 14:49:48 +0300

    Update devServer.js

--- devServer.js
+++ devServer.js
@@ -4,8 +4,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 const path = require('path');
 
-config.entry.app.unshift('webpack-dev-server/client?http://localhost:3000/', 'webpack/hot/dev-server');
-config['devtool'] = 'eval-source-map';
+config.entry.app.unshift('webpack-dev-server/client?http://localhost:9000/', 'webpack/hot/dev-server');
 config['plugins'].push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin()
@@ -13,13 +12,20 @@ config['plugins'].push(
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
+    proxy: {
+        '/api/*': {
+            target: 'http://0.0.0.0:7000/',
+            secure: false,
+            changeOrigin: true
+        }
+    },
     hot: true,
     quiet: false,
     inline: true,
-    historyApiFallback: true,
-}).listen(3000, 'localhost', function (err, result) {
+    historyApiFallback: true
+}).listen(9000, 'localhost', function (err, result) {
     if (err) {
         return console.log(err);
     }
-    console.log('Listening at http://localhost:3000/');
+    console.log('Listening at http://localhost:9000/');
 });
